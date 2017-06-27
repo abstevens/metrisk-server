307af68c8654a1a8261a3469a4706476220e343c
Alexander Stevens <alexanderstevens@192.168.2.4>
2017-06-27 18:51:48 +0300

    Added TrustedProxyServiceProvider

--- config/app.php
+++ config/app.php
@@ -191,6 +191,11 @@ return [
          */
         Laravel\Passport\PassportServiceProvider::class,
 
+        /*
+         * Trusted Proxy Service Providers...
+         */
+        Fideloper\Proxy\TrustedProxyServiceProvider::class,
+
     ],
 
     /*
