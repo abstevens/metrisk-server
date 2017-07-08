3a01a0f253d89c05afe13b8c69196eb4ead12bb3
Alexander Stevens <alexanderstevens@Alexanders-MBP.mshome.net>
2017-07-08 13:29:11 +0300

    Created Classes table migration

---
+++ database/migrations/2017_07_08_100648_create_classes_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClassesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('classes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->integer('order');
+            $table->integer('question_id')->unsigned();
+            $table->integer('author_id')->unsigned();
+
+            $table->foreign('question_id')->references('id')->on('questions');
+            $table->foreign('author_id')->references('id')->on('users');
+
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('classes');
+    }
+}
