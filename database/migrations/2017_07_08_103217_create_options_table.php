0fb3dabbe07d764da05828ffb06f7c5cadfeea5d
Alexander Stevens <alexanderstevens@Alexanders-MBP.mshome.net>
2017-07-08 13:35:18 +0300

    Created Options table migration

---
+++ database/migrations/2017_07_08_103217_create_options_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOptionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('options', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->integer('question_id')->unsigned();
+            $table->integer('order');
+            $table->integer('author_id')->unsigned();
+
+            $table->foreign('question_id')->references('id')->on('questions');
+            $table->foreign('author_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('options');
+    }
+}
