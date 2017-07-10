098db2127c5ea7aeea7f63d556a9a3a6db3586e5
Alexander Stevens <alexanderstevens@Alexanders-MacBook-Pro.local>
2017-07-10 22:31:11 +0300

    Updated User Model with correct table name, fillables and relations

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Laravel\Passport\HasApiTokens;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -12,21 +14,87 @@ class User extends Authenticatable
 {
     use HasApiTokens, Notifiable, CrudTrait, HasRoles;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    /*
+	|--------------------------------------------------------------------------
+	| GLOBAL VARIABLES
+	|--------------------------------------------------------------------------
+	*/
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    protected $table = 'users';
+    protected $primaryKey = 'id';
+    // protected $guarded = [];
+     protected $hidden = ['password', 'remember_token'];
+    protected $fillable = ['name', 'email', 'password'];
+    public $timestamps = true;
+
+    /*
+    |--------------------------------------------------------------------------
+    | FUNCTIONS
+    |--------------------------------------------------------------------------
+    */
+
+    /*
+    |--------------------------------------------------------------------------
+    | RELATIONS
+    |--------------------------------------------------------------------------
+    */
+
+    public function questions(): HasMany
+    {
+        return $this->hasMany('App\Models\Question');
+    }
+
+    public function options(): HasMany
+    {
+        return $this->hasMany('App\Models\Option');
+    }
+
+    public function classes(): HasMany
+    {
+        return $this->hasMany('App\Models\QuestionClass');
+    }
+
+    public function weights(): HasMany
+    {
+        return $this->hasMany('App\Models\Weight');
+    }
+
+    public function categories(): HasMany
+    {
+        return $this->hasMany('App\Models\Category');
+    }
+
+    public function answers(): HasMany
+    {
+        return $this->hasMany('App\Models\Answer');
+    }
+
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany('App\Models\Role');
+    }
+
+    public function permissions(): BelongsToMany
+    {
+        return $this->belongsToMany('App\Models\Permission');
+    }
+
+    /*
+    |--------------------------------------------------------------------------
+    | SCOPES
+    |--------------------------------------------------------------------------
+    */
+
+    /*
+    |--------------------------------------------------------------------------
+    | ACCESORS
+    |--------------------------------------------------------------------------
+    */
+
+    /*
+    |--------------------------------------------------------------------------
+    | MUTATORS
+    |--------------------------------------------------------------------------
+    */
+
 }
